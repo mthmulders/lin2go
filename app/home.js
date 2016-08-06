0744e30592f1421817870c5a3e208fb0561b84b1
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-08-06 22:46:23 +0200

    Prepare for starting a new game

--- app/home.js
+++ app/home.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Actions } from 'react-native-router-flux'
+import Button from 'apsl-react-native-button'
 
-import styles from './styles'
+import globalStyles from './styles'
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    fontSize: 18,
+    marginLeft: 20,
+    marginRight: 20
+  },
+  header: {
+    fontSize: 36,
+    marginBottom: 40
+  }
+});
+
+const startNewGame = function() {
+  console.log('Will start a new game here!');
+};
 
 export default function (props) {
   return (
-    <View style={ styles.container }>
-      <Text>LIN2GO</Text>
+    <View style={ globalStyles.container }>
+      <Text style={ styles.header }>LIN2GO</Text>
+      <Button style={ styles.button } onPress={ startNewGame }>
+        <Text>Start new game</Text>
+      </Button>
     </View>
   );
 };
