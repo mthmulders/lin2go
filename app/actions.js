4f9c59d926aa28e77046780dfa602b9e4fa332f0
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-08-16 08:18:28 +0200

    Directly export constants and functions

--- app/actions.js
+++ app/actions.js
@@ -1,34 +1,21 @@
-const ADD_LETTER_TO_GUESS = 'ADD_LETTER_TO_GUESS';
-const CANCEL_GAME = 'CANCEL_GAME';
-const HIDE_STATS = 'HIDE_STATS';
-const SHOW_STATS = 'SHOW_STATS';
-const START_GAME = 'START_GAME';
+export const ADD_LETTER_TO_GUESS = 'ADD_LETTER_TO_GUESS';
+export const CANCEL_GAME = 'CANCEL_GAME';
+export const HIDE_STATS = 'HIDE_STATS';
+export const SHOW_STATS = 'SHOW_STATS';
+export const START_GAME = 'START_GAME';
 
-const addLetterToGuess = (letter) => {
+export const addLetterToGuess = (letter) => {
   return { type: ADD_LETTER_TO_GUESS, letter: letter };
 };
-const cancelGame = () => {
+export const cancelGame = () => {
   return { type: CANCEL_GAME };
 };
-const hideStats = () => {
+export const hideStats = () => {
   return { type: HIDE_STATS };
 }
-const showStats = () => {
+export const showStats = () => {
   return { type: SHOW_STATS };
 };
-const startGame = () => {
+export const startGame = () => {
   return { type: START_GAME };
-};
-
-export {
-  ADD_LETTER_TO_GUESS,
-  CANCEL_GAME,
-  HIDE_STATS,
-  SHOW_STATS,
-  START_GAME,
-  addLetterToGuess,
-  cancelGame,
-  hideStats,
-  showStats,
-  startGame
 };
