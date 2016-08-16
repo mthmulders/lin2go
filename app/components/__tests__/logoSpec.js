b2c09f0405906ee1f31fa632378aba6cdd721452
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-08-16 07:46:49 +0200

    Simplify test

--- app/components/__tests__/logoSpec.js
+++ app/components/__tests__/logoSpec.js
@@ -4,22 +4,22 @@ import React from 'react';
 import { Image } from 'react-native';
 import ReactTestUtils from 'react-addons-test-utils';
 
-import { findAllWithType } from 'react-shallow-testutils';
+import { findWithType } from 'react-shallow-testutils';
 
 import Logo from '../logo';
 
 describe('The logo component', () => {
+  const renderer = ReactTestUtils.createRenderer();
+
   it('should show the game logo', () => {
     // Arrange
-    const renderer = ReactTestUtils.createRenderer();
 
     // Act
     renderer.render(<Logo />);
 
     // Assert
-    const result = renderer.getRenderOutput();
-    var sources = findAllWithType(result, Image).map((i) => i.props.source);
-    expect(sources.length).toBe(1)
-    expect(sources[0]).toBe('../resources/logo.png');
+    const output = renderer.getRenderOutput();
+    var image = findWithType(output, Image);
+    expect(image.props.source).toBe('../resources/logo.png');
   });
 });
