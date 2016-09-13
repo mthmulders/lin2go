87b5dc7a734b83b9be47d7922d56262f10d0cc08
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-09-13 16:29:41 +0200

    Upgrade React Native 0.32.0 -> 0.33.0

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,4 @@
 import React, { AppRegistry } from 'react-native';
 import App from './app';
 
-AppRegistry.registerComponent('lin2go', () => App);
+AppRegistry.registerComponent('LIN2GO', () => App);
