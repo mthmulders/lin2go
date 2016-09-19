87a5e2700fffe04ff4df6ede8f0549be1932346e
Maarten Mulders <m.th.mulders@xs4all.nl>
2016-09-19 16:51:58 +0200

    Implement game end (both winning and losing)

--- app/reducers/__tests__/statsSpec.js
+++ app/reducers/__tests__/statsSpec.js
@@ -6,30 +6,6 @@ import reducer from '../stats';
 
 const initalState = { losses: 1, wins: 3 };
 
-describe('The \'CANCEL_GAME\' action', () => {
-  it('should increment the number of losses with 1', () => {
-    // Arrange
-    const action = cancelGame();
-
-    // Act
-    const state = reducer(initalState, action);
-
-    // Assert
-    expect(state.losses).toBe(2);
-  });
-
-  it('should keep the number of wins unchanged', () => {
-    // Arrange
-    const action = cancelGame();
-
-    // Act
-    const state = reducer(initalState, action);
-
-    // Assert
-    expect(state.wins).toBe(3);
-  });
-});
-
 describe('The \'LOOSE_GAME\' action', () => {
   it('should increment the number of losses with 1', () => {
     // Arrange
