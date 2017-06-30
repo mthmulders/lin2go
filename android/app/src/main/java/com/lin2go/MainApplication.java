de8a49e97dc5514bf09346b030ab5a2e43f801ca
Maarten Mulders <m.th.mulders@xs4all.nl>
2017-06-30 16:17:52 +0200

    Revert "Upgrade React-Native 0.40.0 -> 0.41.0"
    This reverts commit c430946bfc1da98658fb0cc720037f8f0e1ff762.

--- android/app/src/main/java/com/lin2go/MainApplication.java
+++ android/app/src/main/java/com/lin2go/MainApplication.java
@@ -17,7 +17,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    public boolean getUseDeveloperSupport() {
+    protected boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
