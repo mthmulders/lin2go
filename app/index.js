90684c2f81e5e2ff9196f286caa34ed5e3c37588
Maarten Mulders <m.th.mulders@xs4all.nl>
2017-10-26 09:39:02 +0200

    Remove redux-logger

--- app/index.js
+++ app/index.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
 import App from './containers/app';
 import reducers from './reducers';
 import styles from './styles'
 
-const logger = createLogger({
-  colors: { action: false, error: false, nextState: false, prevState: false, title: false },
-  duration: true,
-  level: 'info',
-  predicate: (getState, action) => __DEV__
-});
+// const logger = createLogger({
+//   colors: { action: false, error: false, nextState: false, prevState: false, title: false },
+//   duration: true,
+//   level: 'info',
+//   predicate: (getState, action) => __DEV__
+// });
 
-const middleware = applyMiddleware(thunkMiddleware, logger);
+const middleware = applyMiddleware(thunkMiddleware);
 const store = createStore(reducers, {}, middleware);
 
 import { loadHistory } from  './statsStorage'
