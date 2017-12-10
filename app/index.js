4b4619c29e0f744a312d51b7089a179815da5460
Maarten Mulders <m.th.mulders@xs4all.nl>
2017-12-10 21:01:35 +0100

    Fix import style, cleanup old code

--- app/index.js
+++ app/index.js
@@ -2,19 +2,11 @@ import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-// import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 
 import App from './containers/app';
 import reducers from './reducers';
 import styles from './styles'
-
-// const logger = createLogger({
-//   colors: { action: false, error: false, nextState: false, prevState: false, title: false },
-//   duration: true,
-//   level: 'info',
-//   predicate: (getState, action) => __DEV__
-// });
 
 const middleware = applyMiddleware(thunkMiddleware);
 const store = createStore(reducers, {}, middleware);
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, { AppRegistry } from 'react-native';
-import App from './app';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './app/index';
 
 AppRegistry.registerComponent('LIN2GO', () => App);
